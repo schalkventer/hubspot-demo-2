22d5176db230f6c55aec1490eacb3a8d9d759a77
Schalk Venter <venterschalk@gmail.com>
2024-11-13 14:57:53 +0200

    Update endpoints CORS

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,22 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
 
 export const config = {
   runtime: "edge",
 };
 
 const app = new Hono().basePath("/api");
+
+app.use(
+  "*",
+  cors({
+    origin: "https://hubspot-demo-2-api.vercel.app/api/list",
+    allowMethods: ["GET"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    maxAge: 86400,
+  })
+);
 
 app.get("/list", (c) => {
   return c.json(["A", "B", "Hello", "World", "Schalk", "C"]);
